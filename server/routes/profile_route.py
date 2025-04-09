4ed90e4f96f29d49b6f8aa8f3c955532efb1a617
maing1 <faithmaingi1622@gmail.com>
2025-04-09 19:59:35 +0300

    added profile route

--- server/routes/profile_route.py
+++ server/routes/profile_route.py
@@ -1,22 +1,36 @@
-# @app.route('/profile/<int:user_id>', methods=['GET', 'PUT'])
-# def profile(user_id):
-#     user = db.session.get(User, user_id)
-#     if not user:
-#         return {"error": "User not found"}, 404
+from flask import jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from models import User, db
 
-#     if request.method == 'GET':
-#         return {
-#             "username": user.username,
-#             "email": user.email,
-#             "bio": user.bio,
-#             "profile_picture": user.profile_picture
-#         }, 200
+def init_profile_route(app):
+    @app.route('/profile', methods=['GET'])
+    @jwt_required()
+    def get_profile():
+        user_id = get_jwt_identity()
+        user = db.session.get(User, user_id)
+        if not user:
+            return {"error": "User not found"}, 404
 
-#     if request.method == 'PUT':
-#         data = request.get_json()
-#         if "bio" in data:
-#             user.bio = data["bio"]
-#         if "profile_picture" in data:
-#             user.profile_picture = data["profile_picture"]
-#         db.session.commit()
-#         return {"message": "Profile updated successfully"}, 200
+        return jsonify({
+                "username": user.username,
+                "email": user.email,
+                "bio": user.bio,
+                "profile_picture": user.profile_picture
+            }), 200
+
+    @app.route('/profile', methods=['PUT'])
+    @jwt_required()
+    def update_profile():
+        user_id = get_jwt_identity()
+        user = db.session.get(User, user_id)
+        if not user:
+            return {"error": "User not found"}, 404
+        
+        data = request.get_json()
+        if "bio" in data:
+            user.bio = data["bio"]
+        if "profile_picture" in data:
+            user.profile_picture = data["profile_picture"]
+            
+        db.session.commit()
+        return {"message": "Profile updated successfully"}, 200
